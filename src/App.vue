e2bd6dd8b9af8cd37f2f2fdc741a2badc3044d40
Noviiich <nowikoff63@gmail.com>
2024-03-29 18:46:08 +0300

    fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,15 @@
-<script>
-let apiKey = "20dd21edc24ea3b46733e9fc4989774f";
-let apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
-let ipUrl = "https://ipapi.co/json/";
-export default {
-  data() {
-    return {
-      name: "clouds",
-      tempCity: "",
-      city: "",
-      error: "",
-      humidity: "",
-      pressure: "",
-      celsius: ""
-    }
-  },
-  computed: {
-    getImg() {
-      return new URL(`./assets/images/${this.name}.png`, import.meta.url).href;
-    }
-  },
-  created() {
-    console.log("awd");
-    this.checkByIP();
-  },
-  methods: {
-    async getWeather(val) {
-      try{
-        const response = await fetch(`${apiUrl}&q=${val}&appid=${apiKey}`);
-        if (!response.ok) {
-            throw new Error("Не удается получить данные о погоде")
-        }
-
-        const data = await response.json();
-        this.city = data.name;
-        this.celsius = Math.round(data.main.temp);
-        this.humidity = data.main.humidity + "%";
-        this.pressure = data.main.pressure;
-
-        if (data.weather[0].main === "Clouds") {
-            this.name = "clouds";
-          } else if (data.weather[0].main === "Clear") {
-            this.name = "clear";
-          } else if (data.weather[0].main === "Rain") {
-            this.name = "rain";
-          } else if (data.weather[0].main === "Drizzle") {
-            this.name = "drizzle";
-          } else if (data.weather[0].main === "Mist") {
-            this.name = "mist";
-          }
-      } catch (e){
-          this.error = e;
-      }
-  },
-    async checkByIP() {
-      try {
-        const response = await fetch(`${ipUrl}`);
-        if (!response.ok) {
-            throw new Error("Ошибка в apiUrl")
-        }
-
-        const data = await response.json();
-        if (data.city !==  "") {
-          this.city = data.city;
-          this.getWeather(this.city);
-        }
-
-      } catch (e) {
-          this.error = e;
-      }
-    }
-}
-}
-</script>
-
 <template>
   <div class="card">
     <h1>Погода</h1>
     <div class="search">
       <input type="text" v-model="tempCity" placeholder="Введите название города">
-      <button @click="getWeather(this.tempCity)">поиск</button>
+      <button @click="getWeather(tempCity)">поиск</button>
     </div>
 
-    <p class="error">{{ error }}</p>
+    <p v-show="error" class="error">{{ error }}</p>
 
-    <div class="weather" v-show="city"> 
-      <img :src="getImg" alt="img" class="weather-icon"/> 
+    <div class="weather-container" v-show="city"> 
+      <img :src="img" alt="img" class="weather-icon"/> 
       <h1 class="temp">{{ celsius + "°C"}}</h1>
       <h2 class="city">{{ city }}</h2>
 
@@ -114,11 +39,68 @@ export default {
       </div>
 
     </div>
-
-    
   </div>
 
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "clouds",
+      tempCity: "",
+      city: "",
+      error: "",
+      humidity: "",
+      pressure: "",
+      celsius: ""
+    }
+  },
+  computed: {
+    img() {
+      return new URL(`./assets/images/${this.name}.png`, import.meta.url).href;
+    }
+  },
+  created() {
+    this.checkByIP();
+  },
+  methods: {
+    async getWeather(val) {
+      try{
+        let response = await fetch(`${import.meta.env.VITE_API_URL}&q=${val}&appid=${import.meta.env.VITE_API_KEY}`);
+        if (!response.ok) {
+            throw new Error("Не удается получить данные о погоде")
+        }
+        this.error = "";
+        let data = await response.json();
+        this.city = data.name;
+        this.celsius = Math.round(data.main.temp);
+        this.humidity = data.main.humidity + "%";
+        this.pressure = data.main.pressure;
+	      this.name = data.weather[0].main.toLowerCase();
+      } catch (e){
+          this.error = e;
+      }
+  },
+    async checkByIP() {
+      try {
+        let response = await fetch(`${import.meta.env.VITE_IP_URL}`);
+        if (!response.ok) {
+            throw new Error("Ошибка в apiUrl")
+        }
+        let data = await response.json();
+        if (data.city !==  "") {
+          this.city = data.city;
+          this.getWeather(this.city);
+        }
+
+      } catch (e) {
+          this.error = e;
+      }
+    }
+}
+}
+</script>
 
 <style scoped>
 
